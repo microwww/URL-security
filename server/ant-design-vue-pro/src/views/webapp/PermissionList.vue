605f466c79c925eb40c0f36fa87e4b40025e8c28
changshu.li <lcs.005@163.com>
2020-12-10 16:22:14 +0800

    permission  edit / webapp

--- server/ant-design-vue-pro/src/views/webapp/PermissionList.vue
+++ server/ant-design-vue-pro/src/views/webapp/PermissionList.vue
@@ -53,14 +53,74 @@
         </span>
       </s-table>
     </a-card>
+
+    <div>
+      <a-modal v-model="domain.visible" title="edit" :footer="null">
+        <a-form :form="editForm" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }" @submit="editSubmit">
+          <a-form-item label="名称">
+            <a-input
+              v-decorator="['name', { rules: [{ required: true, message: 'Please input your note!' }] }]"
+            />
+          </a-form-item>
+          <a-form-item label="应用">
+            <a-select
+              show-search
+              v-decorator="['webapp.id', { rules: [{ required: true, message: 'Please input your note!' }] }]"
+              placeholder="input search text"
+              :default-active-first-option="false"
+              :show-arrow="false"
+              :filter-option="false"
+              :loading="domain.loading"
+              @search="searchWebapp"
+              @change="selectWebapp"
+            >
+              <a-select-option v-for="d in domain.searchData" :key="d.id">
+                {{ d.name }}
+              </a-select-option>
+            </a-select>
+          </a-form-item>
+          <a-form-item label="类型">
+            <a-radio-group
+              v-decorator="['type', { initialValue: false, rules: [{ required: true, message: 'Please input your note!' }] }]"
+            >
+              <a-radio value="URL">
+                URL
+              </a-radio>
+              <a-radio value="MENU">
+                菜单
+              </a-radio>
+            </a-radio-group>
+          </a-form-item>
+          <a-form-item label="URI">
+            <a-input
+              v-decorator="['uri', { rules: [{ message: 'Please input your note!' }] }]"
+            />
+          </a-form-item>
+          <a-form-item label="父菜单">
+            <a-input
+              v-decorator="['parent.id', { type: 'email', rules: [{ message: 'Please input your note!' }] }]"
+            />
+          </a-form-item>
+          <a-form-item label="描述">
+            <a-input
+              v-decorator="['description', { type: 'email', rules: [{ message: 'Please input your note!' }] }]"
+            />
+          </a-form-item>
+          <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
+            <a-button type="primary" html-type="submit">Submit</a-button>
+          </a-form-item>
+        </a-form>
+      </a-modal>
+    </div>
   </page-header-wrapper>
 </template>
 
 <script>
 import moment from 'moment'
 import { STable, Ellipsis } from '@/components'
-import { listPermission, trimObject } from '@/api/entity'
+import { listPermission, listWebapp, savePermission, trimObject, axios } from '@/api/entity'
 
+const CancelToken = axios.CancelToken
 const columns = [
   {
     title: 'ID',
@@ -111,8 +171,15 @@ export default {
   data () {
     this.columns = columns
     return {
-      // create model
-      visible: false,
+      domain: {
+        obj: {},
+        wait: null,
+        cancel: () => {},
+        searchData: [],
+        loading: false,
+        visible: false
+      },
+      editForm: this.$form.createForm(this),
       confirmLoading: false,
       mdl: null,
       // 高级搜索 展开/关闭
@@ -158,13 +225,76 @@ export default {
     }
   },
   methods: {
-    handleAdd () {
-      this.mdl = null
-      this.visible = true
+    selectWebapp (v, opt) {
+      this.domain.obj.webapp = { id: v }
+    },
+    searchWebapp (v) {
+      if (!v) return
+      this.domain.cancel('')
+      clearTimeout(this.domain.wait)
+      this.domain.loading = false
+      this.domain.wait = setTimeout(() => {
+        this.domain.loading = true
+        this.search(v).finally(() => { this.domain.loading = false })
+      }, 1000)
+    },
+    search (v) {
+      const param = {}
+      param.name = {
+        key: 'name',
+        val: '%' + v + '%',
+        opt: 'like'
+      }
+      const the = this
+      return listWebapp({ query: param }, {
+        cancelToken: new CancelToken(cancel => {
+          the.domain.cancel = cancel
+        })
+      }).then((res) => {
+        const data = []
+        res.content.forEach((e) => {
+          data.push(e)
+        })
+        this.domain.searchData = data
+      }).catch((e) => {})
     },
     handleEdit (record) {
-      this.visible = true
-      this.mdl = { ...record }
+      const cp = Object.assign({}, record)
+      this.domain.visible = true
+      this.domain.searchData = [ cp.webapp ]
+      this.$nextTick(() => {
+        this.domain.obj = cp
+        this.showEdit(cp)
+      })
+    },
+    showEdit (record) {
+      const rc = Object.assign({}, record, { webapp: { id: record.webapp.id, name: record.webapp.name } })
+      delete rc.id
+      delete rc.createTime
+      console.log(record)
+      this.editForm.setFieldsValue(rc)
+    },
+    handleAdd () {
+      this.domain.visible = true
+      this.domain.obj = {}
+      this.domain.searchData = []
+      this.editForm.resetFields()
+    },
+    editSubmit (e) {
+      e.preventDefault()
+      this.editForm.validateFields((err, values) => {
+        if (!err) {
+          // console.log('Received values of form: ', values)
+          const entity = Object.assign({}, this.domain.obj, values)
+          savePermission(entity).then((res) => {
+            this.$refs.table.refresh()
+            this.domain.visible = false
+            this.$message.info('提交成功')
+          }).catch((e) => {
+            this.$message.error('出错了 : ' + e.response.data.message)
+          })
+        }
+      })
     },
     handleOk () {
       const form = this.$refs.createModal.form
